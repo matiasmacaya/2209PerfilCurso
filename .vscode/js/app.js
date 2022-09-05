ccdb9a958486b5158286980239b4929f8182c491
matiasmacaya <matiasmacaya@gmail.com>
2022-09-05 19:52:50 -0300

    Hasta Cl 03

---
+++ .vscode/js/app.js
@@ -0,0 +1,14 @@
+/* Clase02
+let myVar = 0;
+var myVar2 = -1; */
+
+const myDoc = document;
+
+console.log(myDoc);
+
+let myH1 = myDoc.getElementsByTagName("h1");
+
+let myH3 = myDoc.getElementById("mySuperH3");
+
+console.log(myH1);
+console.log(myH3);
